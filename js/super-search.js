8558043af46f14b342566d3fabf5ae890e9eccf4
XiaoPongo <100044523+XiaoPongo@users.noreply.github.com>
2025-05-29 22:20:12 +0530

    Update super-search.js

--- js/super-search.js
+++ js/super-search.js
@@ -11,29 +11,25 @@ MIT Licensed
 		lastSearchResultHash,
 		posts = [];
 
-	// Changes XML to JSON
-	// Modified version from here: http://davidwalsh.name/convert-xml-json
+	// Converts XML to JSON (simplified)
 	function xmlToJson(xml) {
-		// Create the return object
 		var obj = {};
-		if (xml.nodeType == 3) { // text
+		if (xml.nodeType === 3) { // text
 			obj = xml.nodeValue;
 		}
 
-		// do children
-		// If all text nodes inside, get concatenated text from them.
 		var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
 		if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
 			obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
 		}
 		else if (xml.hasChildNodes()) {
-			for(var i = 0; i < xml.childNodes.length; i++) {
+			for (var i = 0; i < xml.childNodes.length; i++) {
 				var item = xml.childNodes.item(i);
 				var nodeName = item.nodeName;
-				if (typeof(obj[nodeName]) == "undefined") {
+				if (typeof (obj[nodeName]) == "undefined") {
 					obj[nodeName] = xmlToJson(item);
 				} else {
-					if (typeof(obj[nodeName].push) == "undefined") {
+					if (typeof (obj[nodeName].push) == "undefined") {
 						var old = obj[nodeName];
 						obj[nodeName] = [];
 						obj[nodeName].push(old);
@@ -42,21 +38,29 @@ MIT Licensed
 				}
 			}
 		}
+		if (xml.attributes && xml.attributes.length > 0) {
+			obj['@attributes'] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
 		return obj;
 	}
 
 	function getPostsFromXml(xml) {
 		var json = xmlToJson(xml);
-		return json.channel.item;
+		if (json.feed && json.feed.entry) {
+			return Array.isArray(json.feed.entry) ? json.feed.entry : [json.feed.entry];
+		}
+		return [];
 	}
 
 	window.toggleSearch = function toggleSearch() {
 		searchEl.classList.toggle('is-active');
 		if (searchEl.classList.contains('is-active')) {
-			// while opening
 			searchInputEl.value = '';
 		} else {
-			// while closing
 			searchResultsEl.classList.add('is-hidden');
 		}
 		setTimeout(function () {
@@ -65,8 +69,6 @@ MIT Licensed
 	}
 
 	function handleInput() {
-		var currentResultHash, d;
-
 		currentInputValue = (searchInputEl.value + '').toLowerCase();
 		if (!currentInputValue || currentInputValue.length < 3) {
 			lastSearchResultHash = '';
@@ -76,19 +78,28 @@ MIT Licensed
 		searchResultsEl.style.offsetWidth;
 
 		var matchingPosts = posts.filter(function (post) {
-			if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
-				return true;
-			}
+			const title = post.title && post.title['#text'] || '';
+			const summary = post.summary && post.summary['#text'] || '';
+			return title.toLowerCase().includes(currentInputValue) || summary.toLowerCase().includes(currentInputValue);
 		});
+
 		if (!matchingPosts.length) {
 			searchResultsEl.classList.add('is-hidden');
+			return;
 		}
-		currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
-		if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
+
+		var currentResultHash = matchingPosts.reduce(function (hash, post) {
+			return (post.title && post.title['#text'] || '') + hash;
+		}, '');
+
+		if (currentResultHash !== lastSearchResultHash) {
 			searchResultsEl.classList.remove('is-hidden');
 			searchResultsEl.innerHTML = matchingPosts.map(function (post) {
-				d = new Date(post.pubDate);
-				return '<li><a href="' + post.link + '">' + post.title + '<span class="super-search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+				const title = post.title['#text'] || '';
+				const link = post.link && post.link['@attributes'] && post.link['@attributes'].href || '#';
+				const dateStr = post.published || post.updated || '';
+				const date = new Date(dateStr).toUTCString();
+				return `<li><a href="${link}">${title}<span class="super-search__result-date">${date}</span></a></li>`;
 			}).join('');
 		}
 		lastSearchResultHash = currentResultHash;
@@ -100,37 +111,27 @@ MIT Licensed
 		searchInputEl = document.querySelector(options.inputSelector || '#js-super-search__input');
 		searchResultsEl = document.querySelector(options.resultsSelector || '#js-super-search__results');
 
-		var xmlhttp=new XMLHttpRequest();
+		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.open('GET', searchFile);
 		xmlhttp.onreadystatechange = function () {
-			if (xmlhttp.readyState != 4) return;
-			if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
+			if (xmlhttp.readyState !== 4) return;
+			if (xmlhttp.status !== 200 && xmlhttp.status !== 304) return;
 			var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
-			node = node.children[0];
 			posts = getPostsFromXml(node);
 		}
 		xmlhttp.send();
 
 		// Toggle on ESC key
-		window.addEventListener('keyup', function onKeyPress(e) {
-			if (e.which === 27) {
-				toggleSearch();
-			}
+		window.addEventListener('keyup', function (e) {
+			if (e.which === 27) toggleSearch();
 		});
 		// Open on '/' key
-		window.addEventListener('keypress', function onKeyPress(e) {
-			if (e.which === 47 && !searchEl.classList.contains('is-active')) {
-				toggleSearch();
-			}
+		window.addEventListener('keypress', function (e) {
+			if (e.which === 47 && !searchEl.classList.contains('is-active')) toggleSearch();
 		});
-
-		searchInputEl.addEventListener('input', function onInputChange() {
-			handleInput();
-		});
+		searchInputEl.addEventListener('input', handleInput);
 	}
 
 	init.toggle = toggleSearch;
-
 	window.superSearch = init;
-
 })();
