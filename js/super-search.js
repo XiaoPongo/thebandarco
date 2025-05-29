fc03ecec05aee6d8c9d19dd728f46d7a3144141c
XiaoPongo <100044523+XiaoPongo@users.noreply.github.com>
2025-05-29 21:23:28 +0530

    Update super-search.js

--- js/super-search.js
+++ js/super-search.js
@@ -1,128 +1,136 @@
-(() => {
-  let posts = [];
-  let searchEl, searchInputEl, searchResultsEl;
-  let currentInputValue = '';
-  let lastSearchResultHash = '';
-  let searchFile = '/search.json'; // default JSON file
+/* super-search
+Author: Kushagra Gour (http://kushagragour.in)
+MIT Licensed
+*/
+(function () {
+	var searchFile = '/feed.xml',
+		searchEl,
+		searchInputEl,
+		searchResultsEl,
+		currentInputValue = '',
+		lastSearchResultHash,
+		posts = [];
 
-  // Format date string to "MMM DD, YYYY"
-  const formatDate = (dateStr) => {
-    if (!dateStr) return '';
-    const date = new Date(dateStr);
-    if (isNaN(date)) return '';
-    return date.toLocaleDateString(undefined, {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric'
-    });
-  };
+	// Changes XML to JSON
+	// Modified version from here: http://davidwalsh.name/convert-xml-json
+	function xmlToJson(xml) {
+		// Create the return object
+		var obj = {};
+		if (xml.nodeType == 3) { // text
+			obj = xml.nodeValue;
+		}
 
-  // Show/hide results based on matching posts
-  const handleInput = () => {
-    currentInputValue = searchInputEl.value.toLowerCase().trim();
+		// do children
+		// If all text nodes inside, get concatenated text from them.
+		var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
+		if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+			obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
+		}
+		else if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName;
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	}
 
-    if (!currentInputValue || currentInputValue.length < 2) {
-      lastSearchResultHash = '';
-      searchResultsEl.classList.add('is-hidden');
-      searchResultsEl.innerHTML = '';
-      return;
-    }
+	function getPostsFromXml(xml) {
+		var json = xmlToJson(xml);
+		return json.channel.item;
+	}
 
-    const terms = currentInputValue.split(/\s+/).filter(Boolean);
+	window.toggleSearch = function toggleSearch() {
+		searchEl.classList.toggle('is-active');
+		if (searchEl.classList.contains('is-active')) {
+			// while opening
+			searchInputEl.value = '';
+		} else {
+			// while closing
+			searchResultsEl.classList.add('is-hidden');
+		}
+		setTimeout(function () {
+			searchInputEl.focus();
+		}, 210);
+	}
 
-    const matchingPosts = posts.filter(post => {
-      const haystack = [
-        post.title,
-        post.category,
-        post.tags,
-        post.content
-      ].filter(Boolean).join(' ').toLowerCase();
+	function handleInput() {
+		var currentResultHash, d;
 
-      const matches = terms.every(term => haystack.includes(term));
-      if (matches) {
-        console.log('MATCH FOUND:', post.title, '->', haystack);
-      }
-      return matches;
-    });
+		currentInputValue = (searchInputEl.value + '').toLowerCase();
+		if (!currentInputValue || currentInputValue.length < 3) {
+			lastSearchResultHash = '';
+			searchResultsEl.classList.add('is-hidden');
+			return;
+		}
+		searchResultsEl.style.offsetWidth;
 
-    const currentResultHash = matchingPosts.map(p => p.title).join('');
+		var matchingPosts = posts.filter(function (post) {
+			if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
+				return true;
+			}
+		});
+		if (!matchingPosts.length) {
+			searchResultsEl.classList.add('is-hidden');
+		}
+		currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
+		if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
+			searchResultsEl.classList.remove('is-hidden');
+			searchResultsEl.innerHTML = matchingPosts.map(function (post) {
+				d = new Date(post.pubDate);
+				return '<li><a href="' + post.link + '">' + post.title + '<span class="super-search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+			}).join('');
+		}
+		lastSearchResultHash = currentResultHash;
+	}
 
-    if (!matchingPosts.length || currentResultHash === lastSearchResultHash) {
-      searchResultsEl.classList.add('is-hidden');
-      searchResultsEl.innerHTML = '';
-      return;
-    }
+	function init(options) {
+		searchFile = options.searchFile || searchFile;
+		searchEl = document.querySelector(options.searchSelector || '#js-super-search');
+		searchInputEl = document.querySelector(options.inputSelector || '#js-super-search__input');
+		searchResultsEl = document.querySelector(options.resultsSelector || '#js-super-search__results');
 
-    searchResultsEl.classList.remove('is-hidden');
+		var xmlhttp=new XMLHttpRequest();
+		xmlhttp.open('GET', searchFile);
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState != 4) return;
+			if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
+			var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
+			node = node.children[0];
+			posts = getPostsFromXml(node);
+		}
+		xmlhttp.send();
 
-    searchResultsEl.innerHTML = matchingPosts.map(post => {
-      const dateStr = formatDate(post.date);
-      return `
-        <li>
-          <a href="${post.url}">
-            ${post.title}
-            ${dateStr ? `<span class="super-search__result-date">${dateStr}</span>` : ''}
-          </a>
-        </li>`;
-    }).join('');
+		// Toggle on ESC key
+		window.addEventListener('keyup', function onKeyPress(e) {
+			if (e.which === 27) {
+				toggleSearch();
+			}
+		});
+		// Open on '/' key
+		window.addEventListener('keypress', function onKeyPress(e) {
+			if (e.which === 47 && !searchEl.classList.contains('is-active')) {
+				toggleSearch();
+			}
+		});
 
-    lastSearchResultHash = currentResultHash;
-  };
+		searchInputEl.addEventListener('input', function onInputChange() {
+			handleInput();
+		});
+	}
 
-  // Main search init
-  const superSearch = ({
-    searchFile: file = '/search.json',
-    searchSelector = '#js-super-search',
-    inputSelector = '#js-super-search__input',
-    resultsSelector = '#js-super-search__results'
-  } = {}) => {
-    searchFile = file;
-    searchEl = document.querySelector(searchSelector);
-    searchInputEl = document.querySelector(inputSelector);
-    searchResultsEl = document.querySelector(resultsSelector);
+	init.toggle = toggleSearch;
 
-    fetch(searchFile)
-      .then(res => res.json())
-      .then(data => {
-        posts = data;
-        console.log('Search index loaded:', posts.length, 'items');
-      })
-      .catch(err => {
-        console.error('Error loading search index:', err);
-      });
+	window.superSearch = init;
 
-    // Keyboard shortcuts
-    window.addEventListener('keyup', e => {
-      if (e.key === 'Escape') toggle();
-    });
-
-    window.addEventListener('keypress', e => {
-      if (e.key === '/' && !searchEl.classList.contains('is-active')) {
-        toggle();
-        e.preventDefault();
-      }
-    });
-
-    searchInputEl?.addEventListener('input', handleInput);
-  };
-
-  // Toggle search box
-  const toggle = () => {
-    if (!searchEl || !searchInputEl || !searchResultsEl) return;
-
-    searchEl.classList.toggle('is-active');
-
-    if (searchEl.classList.contains('is-active')) {
-      searchInputEl.value = '';
-      searchResultsEl.classList.add('is-hidden');
-      searchResultsEl.innerHTML = '';
-      setTimeout(() => searchInputEl.focus(), 200);
-    } else {
-      searchResultsEl.classList.add('is-hidden');
-      searchResultsEl.innerHTML = '';
-    }
-  };
-
-  superSearch.toggle = toggle;
-  window.superSearch = superSearch;
 })();
